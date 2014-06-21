12a4de4b9c555320fce8ba1d6bcfe89878ee3330
mdoneva <veselinova.maria@gmail.com>
2014-06-22 00:00:42 +0300

    Modified TestWorker

--- ExpressionCalculator.Tests/ExpressionCalculator.Tests/src/TestWorker.java
+++ ExpressionCalculator.Tests/ExpressionCalculator.Tests/src/TestWorker.java
@@ -1,17 +1,26 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.InvalidAlgorithmParameterException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.omg.CORBA.DynAnyPackage.InvalidValue;
 
 public class TestWorker {
 	
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+	
 	@Test
 	public void TestWorker_FromFile() throws InvalidValue, InvalidAlgorithmParameterException, IOException, InterruptedException {
-		String path = "/home/maria/Desktop/input.txt";
+		File createdFile = folder.newFile("input.txt");
+		String path = createdFile.getAbsolutePath();
+		String expression = "5 + ((1 + 2) * 4) - 3";
+		Utilities.WriteToFile(path, expression);
 		OutputStream os = null;
 		int numberOfThreads = 1;
 		String expectedResult = "14";
